bbb29baeaa0568dbf6ed66f1cd406237ddf53a1b
trevoryoungPHX <tcyoung4@asu.edu>
2017-12-31 15:29:27 -0700

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => {
     <div className = "footer">
       <div className ="blueBackground">
         <div className = "footerText">
-            &copy; Linked Up 2017 - Developed and Designed by Trevor Young
+            &copy; Linked Up 2017 - Developed and Designed by <a id = "footerText" href = "http://trevdev.co">Trevor Young</a>
         </div>
       </div>
     </div>
